845849ce02875cb76f936209b36fc5a5132cf8d2
jlordo112 <johnlay2@illinois.edu>
2019-12-02 19:47:30 -0600

    you're a lemon

--- app/src/main/java/com/example/geoffit/MainActivity.java
+++ app/src/main/java/com/example/geoffit/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.geoffit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button startButton = findViewById(R.id.startButton);
+        startButton.setOnClickListener(unused -> startGame());
+
+    }
+    private void startGame() {
+        Intent gameIntent = new Intent(this, GameActivity.class);
+        startActivity(gameIntent);
+
     }
 }
-Ge
