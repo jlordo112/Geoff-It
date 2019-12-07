7ff3b66306aeb806c83edab01042d1df41e11ee0
Shawn-Coffell <coffell2@illinois.edu>
2019-12-07 15:33:26 -0600

    yuy8

---
+++ app/src/main/java/com/example/geoffit/LoseActivity.java
@@ -0,0 +1,27 @@
+package com.example.geoffit;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoseActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button startButton = findViewById(R.id.playAgainButton);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startGame();
+            }
+        });
+    }
+    private void startGame() {
+        Intent gameIntent = new Intent(this, GameActivity.class);
+        startActivity(gameIntent);
+
+    }
+}
